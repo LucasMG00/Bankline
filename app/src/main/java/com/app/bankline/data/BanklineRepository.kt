ce596e04d9a3605b910ac0c69520112bd1b7f8b5
Lucas MG <lu.mang.gon@gmail.com>
2022-06-24 17:09:13 -0300

    android core update

--- app/src/main/java/com/app/bankline/data/BanklineRepository.kt
+++ app/src/main/java/com/app/bankline/data/BanklineRepository.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.liveData
 import com.app.bankline.data.remote.BanklineApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object BanklineRepository {
 
